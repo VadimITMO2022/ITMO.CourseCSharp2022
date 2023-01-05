83ab7dc76c04ad8d740f73a80e1eafb3502c4ced
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-01-05 14:51:45 +0300

    Add files via upload

---
+++ Csharp_3.1. BankAccount/BankAccount/Enum.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace bankAccount
+{
+    public enum AccountType { Checking, Deposit }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+
+
+
