3ec6ef37f2a6e2f09975555d30e3440a3cb4618f
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-01-05 14:48:50 +0300

    Add files via upload

---
+++ Csharp_2.1. Greeting/Greeting/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Greeting
+{
+    internal class Program
+    {
+      //  static void Main(string[] args)  
+        static void Main()
+        {
+            string myName;
+            /*   Console.WriteLine("Hello world");
+               Console.ReadKey(); */
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello {0}", myName);
+            Console.ReadKey();
+        }
+    }
+}
