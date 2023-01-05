0822104a3b6080b058b3c6123c415c6d53e10d5a
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-01-05 14:59:24 +0300

    Add files via upload

---
+++ Csharp_8.2.Utils/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Utils
+{
+    internal class Test
+    {
+        static void Main()
+        {
+            string message, str1;
+
+            // Get an input string
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+
+            // Reverse the string
+            Utils.Reverse(message, out str1);
+
+            // Display the result
+            Console.WriteLine(str1);
+
+            Console.Read();
+
+        }
+
+    }
+
+}
+
+
